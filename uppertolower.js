be2df1138ee3eecb0be22bd794b437f5ac536d9f
RANJEET KUMAR <98004611+ranjeetmasaischool@users.noreply.github.com>
2022-05-04 19:08:25 +0530

    Add files via upload

---
+++ uppertolower.js
@@ -0,0 +1,20 @@
+// This function convert Upper Case to lower Case..
+function uppertolower(woed) {
+let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+let lower="abcdefghijklmnopqrstuvwxyz ";
+let bag="";
+for(let i=0;i<word.length;i++){
+  for (let j = 0; j < upper.length; j++) {
+    if(word[i]==upper[j] ||(word[i]==lower[j])){
+      bag=bag+lower[j];
+    }
+  }
+}
+return bag;
+}
+// Write Here Your Word 
+let word="RANJEeT kumar SaH";
+let output=uppertolower(word)
+
+// You use output as a Result.....
+console.log(output);
